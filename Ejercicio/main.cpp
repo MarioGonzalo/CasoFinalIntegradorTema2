8c57cd1c12406014aea7a759aaac87ca95c5c5b5
MarioGonzalo <146821965+MarioGonzalo@users.noreply.github.com>
2023-11-30 11:57:57 +0100

    Completar load_script

--- Ejercicio/main.cpp
+++ Ejercicio/main.cpp
@@ -25,7 +25,24 @@ void load_script(const char* filename, bool show_script = false){
             cerr << "Error: no se pudo abrir el archivo " << filename << endl;
             return;
         }
+
+        int c;
+        char buf[4001];
+        while ((c = fread(buf, 1, 4000, f)) > 0){
+            buf[c] = 0;
+            script.append(buf);
+        }
+        fclose(f);
+        f = nullptr;
+
+        if(show_script){
+            cout << ColorConsole::fg_black << ColorConsole::bg_white;
+            cout << script << endl;
+        }
+        consoleBox->new_text();
+        consoleBox->set_text(script);
     }
+    
 }
 int main() {
     std::cout << "Hello, World!" << std::endl;
